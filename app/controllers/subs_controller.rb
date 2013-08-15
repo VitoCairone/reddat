2a1a3098cd25168a916d3c65d0a0acab4c71a255
Vito Cairone <vitocairone@gmail.com>
2013-08-15 15:24:36 -0700

    Added simultaneous creation of up to 5 Links with Sub creation

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,9 +1,24 @@
 class SubsController < ApplicationController
 
   before_filter :ensure_logged_in, except: [:index, :show]
+  before_filter :ensure_moderator, only: [:edit, :update]
+
+  def ensure_moderator
+    @sub = Sub.find(params[:id])
+    redirect_to :index unless @sub.moderator == current_user.id
+  end
 
   def create
     #params[:sub][:moderator] = current_user.id #if creator is mod by default
+    links_attrs = params[:sub][:links_attributes]
+    5.times do |i|
+      link_attr = links_attrs[i.to_s]
+      if (link_attr[:title].blank? &&
+        link_attr[:url].blank? &&
+        link_attr[:text].blank?)
+        link_attr[:sub_ids] = nil
+      end
+    end
     @sub = Sub.new(params[:sub])
     if @sub.save
       redirect_to sub_url(@sub)
@@ -18,6 +33,7 @@ class SubsController < ApplicationController
   end
 
   def new
+    @allow_simulpost = true
     @sub = Sub.new
     render :new
   end
@@ -27,12 +43,12 @@ class SubsController < ApplicationController
   end
 
   def edit
-    @sub = Sub.find(params[:id])
+    #@sub = Sub.find(params[:id]) # set by filter
     render :edit
   end
 
   def update
-    @sub = Sub.find(params[:id])
+    #@sub = Sub.find(params[:id]) # set by filter
     @sub.update_attributes(params[:sub])
     if @sub.save
       redirect_to sub_url(@sub)
--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,5 +1,5 @@
 class Sub < ActiveRecord::Base
-  attr_accessible :name, :moderator
+  attr_accessible :name, :moderator, :links_attributes
 
   has_many :sub_memberships
 
@@ -8,4 +8,6 @@ class Sub < ActiveRecord::Base
   has_many :links, through: :sub_links, source: :link
 
   has_many :members, through: :sub_memerships, source: :user
+
+  accepts_nested_attributes_for :links, :reject_if => :all_blank
 end
